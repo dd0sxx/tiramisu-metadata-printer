1d626bf6b9fb98043ff40b8ced31d8d35509efe4
dd0sxx <contacttheotelonis@gmail.com>
2022-04-01 13:01:01 -0400

    printer

--- printer.js
+++ printer.js
@@ -2,28 +2,80 @@ const fs = require('fs')
 
 async function print (path) {
     try {
+        let num = 0;
         const files = fs.readdirSync(path);
         for (let i = 0; i < files.length; i++) {
-            const metadata = {
-                name: `Silhouette Punk #${i}`,
-                description: `10,000 Silhouette Punks together create a single conceptual art piece on the Ethereum Blockchain.\n\nSilhouette Punks are conceptual art: They exist to promote thought and ask questions.\n\nAbout values, status, identity, creativity, communication, how we are different, how we are the same.\n\nThey provide no answers. They reflect.\n\nThis art project is not associated with the legendary Crypto Punks which they reference or Larva Labs.\n\nSilhouette Punks are not intended to be a vehicle for speculation. They may have no monetary value.\n\nThey are an idea. Sold or unsold, as an artistic expression, their value is: They exist.\n\nWe hope they add to the conversation.`,
-                image: `${'ipfs://QmSzWSij7BE1zUSXaK88kwhrGXBeeGFLSpuH6H6XGm5LFV/'}${i}.png`,
-                attributes: [
-                    {
-                        "trait_type": "Color", 
-                        "value": "Black"
-                    },
-                    {
-                        "trait_type": "Background", 
-                        "value": "Blue"
-                    },
-                ]
+            console.log(files[i])
+            let files2 = fs.readdirSync(`${path}/${files[i]}`);
+            for(let j = 1; j <= files2.length / 2; j++) {
+                let _json = fs.readFileSync(`${path}/${files[i]}/${j}.json`); 
+                let _png = fs.readFileSync(`${path}/${files[i]}/${j}.png`); 
+                fs.writeFileSync(`./json/${num}.json`, _json);
+                fs.writeFileSync(`./png/${num}.png`, _png);
+                num++
             }
-            fs.writeFileSync(`./json/${i}.json`, JSON.stringify(metadata));
         }
     } catch (error) {
         console.error(`Got an error trying to read dir: ${error.message}`);
     }
 }
 
-print('../../Downloads/0-9999')
+async function clean () {
+    try {
+        const files = fs.readdirSync('./scrambleJSON');
+        for (let i = 0; i < files.length; i++) {
+            // console.log(i)
+            let _json = fs.readFileSync(`./scrambleJSON/${i}.json`);
+            let json = JSON.parse(_json);
+            let gifIndex = [259, 668, 837, 819, 482, 423, 835, 725, 936, 664]
+            if (gifIndex.indexOf(i) !== -1) {
+                console.log(i)
+                json.image = `ipfs://Qmd3QsG1d5yaBpE85KCdDkhZNgm7NrF4Q6oJMRtbokMhH7/${i}.gif`
+            } else {
+                json.image = `ipfs://Qmd3QsG1d5yaBpE85KCdDkhZNgm7NrF4Q6oJMRtbokMhH7/${i}.png`
+            }
+            // delete json.compiler
+            // json.attributes[0].value = "Tiramisu"
+            // json.name = `Tiramisu #${i}`
+            let out = JSON.stringify(json)
+            fs.writeFileSync(`./scrambleJSON/${i}.json`, out)
+        }
+    } catch (error) {
+        console.error(`Got an error trying to read dir: ${error.message}`);
+    }
+}
+async function deDupe () {
+    try {
+        let out = []
+        const files = fs.readdirSync('./json');
+        for (let i = 0; i < files.length; i++) {
+            let _json = fs.readFileSync(`./json/${i}.json`);
+            let json = JSON.parse(_json);
+            for (let j = 0; j < files.length; j++) {
+                if (j !== i) {
+                    let _json2 = fs.readFileSync(`./json/${j}.json`);
+                    let json2 = JSON.parse(_json2);
+                    if (json2 === json) out.push(i, j)
+                }
+            }
+        }
+        console.log(out)
+    } catch (error) {
+        console.error(`Got an error trying to read dir: ${error.message}`);
+    }
+}
+
+function tenRandomValues () {
+    for (let i = 0; i < 10; i++) {
+        let r = Math.floor(Math.random() * 1000)
+        console.log(r)
+    }
+}
+
+// tenRandomValues()
+
+clean()
+
+// deDupe()
+
+// print('../../Downloads/Tiramisu-Final-Art 2')
