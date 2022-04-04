18a69b274775a4446e46e3c62652f9a789d06539
dd0sxx <contacttheotelonis@gmail.com>
2022-04-04 12:20:50 -0400

    cleaned crumbs

--- printer.js
+++ printer.js
@@ -27,15 +27,26 @@ async function clean () {
             // console.log(i)
             let _json = fs.readFileSync(`./scrambleJSON/${i}.json`);
             let json = JSON.parse(_json);
-            let gifIndex = [259, 668, 837, 819, 482, 423, 835, 725, 936, 664]
-            if (gifIndex.indexOf(i) !== -1) {
-                console.log(i)
-                json.image = `ipfs://Qmd3QsG1d5yaBpE85KCdDkhZNgm7NrF4Q6oJMRtbokMhH7/${i}.gif`
-            } else {
-                json.image = `ipfs://Qmd3QsG1d5yaBpE85KCdDkhZNgm7NrF4Q6oJMRtbokMhH7/${i}.png`
-            }
+            // let gifIndex = [259, 668, 837, 819, 482, 423, 835, 725, 936, 664]
+            // if (gifIndex.indexOf(i) !== -1) {
+            //     console.log(i)
+            //     json.image = `ipfs://Qmd3QsG1d5yaBpE85KCdDkhZNgm7NrF4Q6oJMRtbokMhH7/${i}.gif`
+            // } else {
+            //     json.image = `ipfs://Qmd3QsG1d5yaBpE85KCdDkhZNgm7NrF4Q6oJMRtbokMhH7/${i}.png`
+            // }
             // delete json.compiler
             // json.attributes[0].value = "Tiramisu"
+
+            // console.log(i)
+
+
+            if (json.attributes[3]?.value === "crumb_1.png") {json.attributes[3].value = "crumb 1"}
+            if (json.attributes[3]?.value === "crumb_2.png") {json.attributes[3].value = "crumb 2"}
+            if (json.attributes[3]?.value === "crumb_3.png") {json.attributes[3].value = "crumb 3"}
+            if (json.attributes[3]?.value === "crumb_4.png") {json.attributes[3].value = "crumb 4"}
+            if (json.attributes[3]?.value === "crumb_5.png") {json.attributes[3].value = "crumb 5"}
+            if (json.attributes[3]?.value === "crumb_6.png") {json.attributes[3].value = "crumb 6"}
+            
             // json.name = `Tiramisu #${i}`
             let out = JSON.stringify(json)
             fs.writeFileSync(`./scrambleJSON/${i}.json`, out)
